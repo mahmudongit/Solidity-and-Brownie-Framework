ae6ead5d19d2886643ad0622314175b92d01a645
Vane <mahmudonlinemoney@gmail.com>
2026-03-27 15:01:36 +0100

    Enable source publishing during deployment

--- brownie_fund_me/scripts/deploy.py
+++ brownie_fund_me/scripts/deploy.py
@@ -3,7 +3,7 @@ from scripts.helpful_scripts import get_account
 
 def deploy_fund_me():
     accounts = get_account()
-    fund_me = FundMe.deploy({"from": accounts})
+    fund_me = FundMe.deploy({"from": accounts}, publish_source=True)
     print(f"contract deployed to {fund_me.address}")
 
 
